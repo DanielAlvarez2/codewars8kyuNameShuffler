600e2fefa10630f086547a86e0ed5e4536e520d8
Daniel Yllanes <danielyllanes@Daniels-MacBook-Air.local>
2025-01-20 00:41:41 -0500

    initial commit

---
+++ algorithm.js
@@ -0,0 +1,7 @@
+function shuffle(name){
+    return name
+}
+
+console.log("shuffle('John McClane') -> McClane John / ", shuffle('John McClane'))
+console.log("shuffle('Dan Alvarez') -> Alvarez Dan / ", shuffle('Dan Alvarez'))
+console.log("shuffle('Donald Trump') -> Trump Donald / ", shuffle('Trump Donald'))
